42d56230358563d3a5be22280fc74a8a998be227
Marc Branscheid <marc@skillconomy.com>
2021-04-13 20:43:19 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
